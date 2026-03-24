eb1eb0d9d4c470a5c0b673c91dd2343377b7cc46
Vishal Thakur <vishalultralegend612@gmail.com>
2026-03-24 15:16:29 +0530

    changes done

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 connectDB();
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true }));
+
+app.use(cors({
+  origin: "https://kirana-kart-beusvmjg8-vshal-thkurs-projects.vercel.app/",
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
