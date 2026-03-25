1fb7214c2f2421c580430c58ce1f9a62bd54f821
Vishal Thakur <vishalultralegend612@gmail.com>
2026-03-25 11:47:34 +0530

    Changes done

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,14 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 connectDB();
-// app.use(cors({
-//   origin: [
-//     /https:\/\/kiranakart-.*\.vercel\.app$/,
-//     "http://localhost:5173"
-//   ],
-//   credentials: true
-// }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    /https:\/\/kiranakart-.*\.vercel\.app$/,
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+// app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
